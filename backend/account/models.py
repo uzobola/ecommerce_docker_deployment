a43d09aeddc04ad681f0a6711e2194abc0d1152c
Kingman <107763929+KingmanT@users.noreply.github.com>
2024-11-05 19:02:40 -0500

    Update models.py

--- backend/account/models.py
+++ backend/account/models.py
@@ -8,7 +8,7 @@ class StripeModel(models.Model):
     email = models.EmailField(null=True, blank=True)
     name_on_card = models.CharField(max_length=200, null=True, blank=True)
     customer_id = models.CharField(max_length=200, blank=True, null=True)
-    card_number = models.CharField(max_length=16, unique=True, null=True, blank=True)
+    card_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     exp_month = models.CharField(max_length=2, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
     exp_year = models.CharField(max_length=4, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
     card_id = models.TextField(max_length=100, null=True, blank=True)
@@ -39,7 +39,7 @@ class BillingAddress(models.Model):
 class OrderModel(models.Model):
     name = models.CharField(max_length=120)
     ordered_item = models.CharField(max_length=200, null=True, blank=True, default="Not Set")
-    card_number = models.CharField(max_length=16, null=True, blank=True)
+    card_number = models.CharField(max_length=20, null=True, blank=True)
     address = models.CharField(max_length=300, null=True, blank=True)
     paid_status = models.BooleanField(default=False)
     paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
