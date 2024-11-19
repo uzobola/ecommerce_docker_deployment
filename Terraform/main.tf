18578a127d3b04fbbf9ab1ebd27c378fbd19bf1c
Ubuntu <ubuntu@ip-172-31-45-188.ec2.internal>
2024-11-19 06:51:31 +0000

    Updates

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,6 +27,7 @@ module "ec2" {
   alb_health_check_path = var.alb_health_check_path
 }
 
+# RDS: Database Security Group
 resource "aws_security_group" "rds" {
   name        = "rds-sg"
   description = "Security group for RDS"
@@ -60,6 +61,7 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
+# Initialize RDS instance
 resource "aws_db_instance" "main" {
   identifier           = "ecommerce-db"
   engine               = "postgres"
@@ -79,4 +81,59 @@ resource "aws_db_instance" "main" {
     Name = "ecommerce-rds"
   }
 }
+
+
+# New monitoring configuration
+# Get default VPC information
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
+
+# Create VPC Peering connection
+resource "aws_vpc_peering_connection" "monitoring" {
+  peer_vpc_id = data.aws_vpc.default.id
+  vpc_id      = module.vpc.vpc_id
+  auto_accept = true
+
+  tags = {
+    Name = "monitoring-peer"
+  }
+}
+
+
+# Add route to default VPC route table
+resource "aws_route" "default_to_app" {
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.monitoring.id
+}
+
+
+# Add routes to application VPC private route tables
+resource "aws_route" "app_to_default" {
+  route_table_id            = module.vpc.private_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.monitoring.id
+}
+
+
+
+# Add Node Exporter access to application security group
+resource "aws_security_group_rule" "allow_node_exporter" {
+  type              = "ingress"
+  from_port         = 9100
+  to_port           = 9100
+  protocol          = "tcp"
+  security_group_id = module.ec2.app_sg_id
+  cidr_blocks       = [data.aws_vpc.default.cidr_block]
+}
+
 
--- Terraform/modules/vpc/outputs.tf
+++ Terraform/modules/vpc/outputs.tf
@@ -15,3 +15,7 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "private_route_table_id" {
+  description = "Private route table ID"
+  value       = aws_route_table.private.id
+}
